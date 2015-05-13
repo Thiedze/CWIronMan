b8ba5a66062e305ce1f4bc2463067a4380e50fda
thiedze <github@thiems.eu>
2015-05-13 22:02:44 +0200

    Bluetooth serial

--- src/de/thiems/cwironman/MainActivity.java
+++ src/de/thiems/cwironman/MainActivity.java
@@ -1,6 +1,7 @@
 package de.thiems.cwironman;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
@@ -15,6 +16,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -32,13 +34,19 @@ public class MainActivity extends Activity {
 		try {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_main);
-
-			startActivity(new Intent(
-					"android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+			
 			nReceiver = new NotificationReceiver();
 			IntentFilter filter = new IntentFilter();
 			filter.addAction("de.thiems.cwironman.IronManService");
 			registerReceiver(nReceiver, filter);
+			
+			final Button button = (Button) findViewById(R.id.btnActionNotificationListener);
+	        button.setOnClickListener(new View.OnClickListener() {
+	             public void onClick(View v) {
+					startActivity(new Intent(
+							"android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+	             }
+	         });
 
 			TurnOnBluetooth();
 		} catch (Exception e) {
@@ -69,26 +77,39 @@ public class MainActivity extends Activity {
 			Toast.makeText(MainActivity.this,
 					intent.getStringExtra("notification_event"),
 					Toast.LENGTH_LONG).show();
-			new Thread(new ClientThread()).start();
+			
+			PrintWriter writer;
+			try {
+				writer = new PrintWriter(bluetoothSocket.getOutputStream());
+				writer.append(intent.getStringExtra("notification_event"));
+				writer.append("\n\r");
+				writer.flush();
+
+			} catch (IOException e) {
+				Toast.makeText(MainActivity.this, e.toString(),
+						Toast.LENGTH_LONG).show();
+			}
+			//new Thread(new ClientThread()).start();
 		}
 	}
 
 	@Override
-    protected void onStart() {
-        super.onStart();
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-        RefreshDeviceList();
-    }
-	
+	protected void onStart() {
+		super.onStart();
+		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (!bluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		RefreshDeviceList();
+	}
+
 	public void RefreshDeviceList() {
 		final ArrayList<BluetoothDevice> list = new ArrayList<BluetoothDevice>(
 				bluetoothAdapter.getBondedDevices());
-		final BluetoothArrayAdapter adapter = new BluetoothArrayAdapter(
-				this, android.R.layout.simple_list_item_1, list);
+		final BluetoothArrayAdapter adapter = new BluetoothArrayAdapter(this,
+				android.R.layout.simple_list_item_1, list);
 
 		ListView listview = (ListView) findViewById(R.id.lvBluetoothDevices);
 		listview.setAdapter(adapter);
@@ -107,14 +128,16 @@ public class MainActivity extends Activity {
 					m = item.getClass().getMethod("createRfcommSocket",
 							new Class[] { int.class });
 				} catch (NoSuchMethodException e) {
-					e.printStackTrace();
+					Toast.makeText(MainActivity.this, e.toString(),
+							Toast.LENGTH_LONG).show();
 				}
 
 				try {
 					bluetoothSocket = (BluetoothSocket) m.invoke(
 							bluetoothDevice, 1);
 				} catch (Exception e) {
-					e.printStackTrace();
+					Toast.makeText(MainActivity.this, e.toString(),
+							Toast.LENGTH_LONG).show();
 				}
 
 				try {
@@ -122,31 +145,20 @@ public class MainActivity extends Activity {
 					// until it succeeds or throws an exception
 					bluetoothSocket.connect();
 				} catch (IOException connectException) {
+					Toast.makeText(MainActivity.this,
+							connectException.toString(), Toast.LENGTH_LONG)
+							.show();
 					// Unable to connect; close the socket and get out
 					try {
 						bluetoothSocket.close();
 					} catch (IOException closeException) {
+						Toast.makeText(MainActivity.this,
+								closeException.toString(), Toast.LENGTH_LONG)
+								.show();
 					}
 					return;
 				}
 			}
 		});
-	}
-
-	class ClientThread implements Runnable {
-
-		public ClientThread() {
-
-		}
-
-		@Override
-		public void run() {
-
-		}
-
-		public void cancel() {
-
-		}
-
 	}
 }
