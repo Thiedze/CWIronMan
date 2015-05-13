16828bf703882ee71901cf8f7fdd3ba4ecdde0b7
thiedze <github@thiems.eu>
2015-05-13 19:53:50 +0200

    Add bluetooth alpha state

--- src/de/thiems/cwironman/IronManService.java
+++ src/de/thiems/cwironman/IronManService.java
@@ -3,20 +3,12 @@ package de.thiems.cwironman;
 import android.content.Intent;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.util.Log;
 
 public class IronManService extends NotificationListenerService {
-
-	private String TAG = this.getClass().getSimpleName();
 
 	@Override
 	public void onNotificationPosted(StatusBarNotification sbn) {
 		if (sbn.getPackageName().equals("com.whatsapp")) {
-			Log.i(TAG, "**********  onNotificationPosted");
-			Log.i(TAG,
-					"ID :" + sbn.getId() + "\t"
-							+ sbn.getNotification().tickerText + "\t"
-							+ sbn.getPackageName());
 			Intent i = new Intent("de.thiems.cwironman.IronManService");
 			i.putExtra("notification_event",
 					"onNotificationPosted :" + sbn.getPackageName() + "\n");
@@ -27,11 +19,6 @@ public class IronManService extends NotificationListenerService {
 	@Override
 	public void onNotificationRemoved(StatusBarNotification sbn) {
 		if (sbn.getPackageName().equals("com.whatsapp")) {
-			Log.i(TAG, "********** onNOtificationRemoved");
-			Log.i(TAG,
-					"ID :" + sbn.getId() + "\t"
-							+ sbn.getNotification().tickerText + "\t"
-							+ sbn.getPackageName());
 			Intent i = new Intent("de.thiems.cwironman.IronManService");
 			i.putExtra("notification_event",
 					"onNotificationRemoved :" + sbn.getPackageName() + "\n");
--- src/de/thiems/cwironman/MainActivity.java
+++ src/de/thiems/cwironman/MainActivity.java
@@ -1,28 +1,31 @@
 package de.thiems.cwironman;
 
 import java.io.IOException;
-import java.util.Set;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private NotificationReceiver nReceiver;
 	private BluetoothAdapter bluetoothAdapter;
+	private BluetoothSocket bluetoothSocket;
+	private BluetoothDevice bluetoothDevice;
 
-	// private Set<BluetoothDevice> pairedDevices;
+	final int REQUEST_ENABLE_BT = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,183 +39,113 @@ public class MainActivity extends Activity {
 			IntentFilter filter = new IntentFilter();
 			filter.addAction("de.thiems.cwironman.IronManService");
 			registerReceiver(nReceiver, filter);
-			
-			InitBluetooth();
+
+			TurnOnBluetooth();
 		} catch (Exception e) {
 			Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG)
 					.show();
 		}
 
 	}
-	
+
 	private void TurnOnBluetooth() {
+		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
 		if (!bluetoothAdapter.isEnabled()) {
 			Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(turnOn, 0);
-			Toast.makeText(getApplicationContext(), "Turned on",
+			Toast.makeText(getApplicationContext(), "Bluetooth turned on",
 					Toast.LENGTH_LONG).show();
 		} else {
-			Toast.makeText(getApplicationContext(), "Already on",
+			Toast.makeText(getApplicationContext(), "Bluetooth already on",
 					Toast.LENGTH_LONG).show();
 		}
 	}
-
-	/**
-     * Start device discover with the BluetoothAdapter
-     */
-    private void doDiscovery() {
-        //if (D) Log.d(TAG, "doDiscovery()");
-
-        // Indicate scanning in the title
-        setProgressBarIndeterminateVisibility(true);
-        //setTitle(R.string.scanning);
-
-        // Turn on sub-title for new devices
-        //findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
-
-        // If we're already discovering, stop it
-        if (bluetoothAdapter.isDiscovering()) {
-        	bluetoothAdapter.cancelDiscovery();
-        }
-
-        // Request discover from BluetoothAdapter
-        bluetoothAdapter.startDiscovery();
-    }
-	
-	private void InitBluetooth() {		
-		// Get the local Bluetooth adapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		
-		TurnOnBluetooth();
-		
-		// Set result CANCELED incase the user backs out
-        setResult(Activity.RESULT_CANCELED);
-        
-     // Initialize array adapters. One for already paired devices and
-        // one for newly discovered devices
-        //mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-        //mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
-
-        // Find and set up the ListView for paired devices
-//        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
-//        pairedListView.setAdapter(mPairedDevicesArrayAdapter);
-//        pairedListView.setOnItemClickListener(mDeviceClickListener);
-
-        // Find and set up the ListView for newly discovered devices
-//        ListView newDevicesListView = (ListView) findViewById(R.id.new_devices);
-//        newDevicesListView.setAdapter(mNewDevicesArrayAdapter);
-//        newDevicesListView.setOnItemClickListener(mDeviceClickListener);
-
-        // Register for broadcasts when a device is discovered
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        this.registerReceiver(mReceiver, filter);
-
-        // Register for broadcasts when discovery has finished
-        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        this.registerReceiver(mReceiver, filter);
-
-        // Get a set of currently paired devices
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-
-        // If there are paired devices, add each one to the ArrayAdapter
-//        if (pairedDevices.size() > 0) {
-//            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
-//            for (BluetoothDevice device : pairedDevices) {
-//                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-//            }
-//        } else {
-//            String noDevices = getResources().getText(R.string.none_paired).toString();
-//            mPairedDevicesArrayAdapter.add(noDevices);
-//        }
-        
-        doDiscovery();
-	}
-	
-	// The BroadcastReceiver that listens for discovered devices and
-    // changes the title when discovery is finished
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                // If it's already paired, skip it, because it's been listed already
-                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                    //mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                }
-            // When discovery is finished, change the Activity title
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                setProgressBarIndeterminateVisibility(false);
-//                setTitle(R.string.select_device);
-//                if (mNewDevicesArrayAdapter.getCount() == 0) {
-//                    String noDevices = getResources().getText(R.string.none_found).toString();
-//                    mNewDevicesArrayAdapter.add(noDevices);
-//                }
-            }
-        }
-    };
 
 	class NotificationReceiver extends BroadcastReceiver {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
+			Toast.makeText(MainActivity.this,
+					intent.getStringExtra("notification_event"),
+					Toast.LENGTH_LONG).show();
 			new Thread(new ClientThread()).start();
 		}
 	}
 
+	@Override
+    protected void onStart() {
+        super.onStart();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        RefreshDeviceList();
+    }
+	
+	public void RefreshDeviceList() {
+		final ArrayList<BluetoothDevice> list = new ArrayList<BluetoothDevice>(
+				bluetoothAdapter.getBondedDevices());
+		final BluetoothArrayAdapter adapter = new BluetoothArrayAdapter(
+				this, android.R.layout.simple_list_item_1, list);
+
+		ListView listview = (ListView) findViewById(R.id.lvBluetoothDevices);
+		listview.setAdapter(adapter);
+
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, final View view,
+					int position, long id) {
+				final BluetoothDevice item = (BluetoothDevice) parent
+						.getItemAtPosition(position);
+
+				bluetoothDevice = item;
+				Method m = null;
+				try {
+					m = item.getClass().getMethod("createRfcommSocket",
+							new Class[] { int.class });
+				} catch (NoSuchMethodException e) {
+					e.printStackTrace();
+				}
+
+				try {
+					bluetoothSocket = (BluetoothSocket) m.invoke(
+							bluetoothDevice, 1);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				try {
+					// Connect the device through the socket. This will block
+					// until it succeeds or throws an exception
+					bluetoothSocket.connect();
+				} catch (IOException connectException) {
+					// Unable to connect; close the socket and get out
+					try {
+						bluetoothSocket.close();
+					} catch (IOException closeException) {
+					}
+					return;
+				}
+			}
+		});
+	}
+
 	class ClientThread implements Runnable {
 
-		private final BluetoothServerSocket mmServerSocket;
-
 		public ClientThread() {
-			// Use a temporary object that is later assigned to mmServerSocket,
-			// because mmServerSocket is final
-			BluetoothServerSocket tmp = null;
-			// try {
-			// // MY_UUID is the app's UUID string, also used by the client
-			// // code
-			// //tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(
-			// // NAME, MY_UUID);
-			// } catch (IOException e) {
-			// }
-			mmServerSocket = tmp;
+
 		}
 
 		@Override
 		public void run() {
-			try {
-				BluetoothSocket socket = null;
-				// Keep listening until exception occurs or a socket is returned
-				while (true) {
-					try {
-						socket = mmServerSocket.accept();
-					} catch (IOException e) {
-						break;
-					}
-					// If a connection was accepted
-					if (socket != null) {
-						// Do work to manage the connection (in a separate
-						// thread)
-						// manageConnectedSocket(socket);
-						mmServerSocket.close();
-						break;
-					}
-				}
 
-			} catch (Exception e) {
-				return;
-			}
 		}
 
-		/** Will cancel the listening socket, and cause the thread to finish */
 		public void cancel() {
-			try {
-				mmServerSocket.close();
-			} catch (IOException e) {
-			}
+
 		}
 
 	}
